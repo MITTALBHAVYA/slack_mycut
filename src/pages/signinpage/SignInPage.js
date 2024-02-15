4186e5b2552b5378887cab35a888a0c41bfc1a41
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-02-15 15:51:08 +0530

    SignInPage update

--- src/pages/signinpage/SignInPage.js
+++ src/pages/signinpage/SignInPage.js
@@ -19,8 +19,21 @@ const SignInPage = () => {
           </div>
         </div>
       </nav>
-      <h1>Sign In Page</h1>
       {/* Additional sign-in page content */}
+       {/* Centered container with content */}
+       <div className="centered-container">
+        <h1 className='signin-msg'>Sign in to Slack</h1>
+        <p className='signin-sugg'>We suggest using the email address that you use at work</p>
+        
+        {/* Your form or sign-in content can go here */}
+          <button className='signin-ggl'><img src='./images/google_icon.png' alt='G' className='gimg'/><span>Sign in with google</span></button>
+          <span className='or'>OR</span>
+        <form>
+        <input type="text" placeholder="name@workemail.com" className="signin-email"/>
+          <button type="submit" className="sub-btn">Sign in with Email</button>
+        </form>
+        {/* Or, if you prefer buttons separately */}
+      </div>
     </PageLayout>
   );
 };
