3256d563edf87287b1861ab01789fdb6edc86682
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-02-18 15:47:47 +0530

    change in logic of SpaceRobot

--- src/components/SpaceRobot/SpaceRobot.js
+++ src/components/SpaceRobot/SpaceRobot.js
@@ -5,29 +5,50 @@ import FloatingRobotImage from '../SpaceRobot/FloatingRobot.png';
 const SpaceRobot = () => {
   const [position, setPosition] = useState({ top: '0', left: '0' });
   const [size, setSize] = useState('50px');
-  const [direction, setDirection] = useState('right');
+  const [rotation, setRotation] = useState(0);
+  const [linearTransition, setLinearTransition] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const newPosition = {
-        top: Math.random() * window.innerHeight + 'px',
-        left: Math.random() * window.innerWidth + 'px'
-      };
-      const newSize = Math.random() * 100 + 'px';
-      const newDirection = Math.random() > 0.5 ? 'right' : 'left';
+      // Enable linear transition
+      setLinearTransition(true);
 
-      setPosition(newPosition);
-      setSize(newSize);
-      setDirection(newDirection);
-    }, 5000);
+      setTimeout(() => {
+        // Reset rotation and disable linear transition
+        setRotation(0);
+        setLinearTransition(false);
+
+        // After the delay, rotate with a random amount between 36 and 360 degrees
+        const randomRotation = Math.random() * (360 - 36) + 36;
+        setRotation(randomRotation);
+
+        setTimeout(() => {
+          // After rotation, set new position
+          const newPosition = {
+            top: (Math.random() * window.innerHeight) * 0.75 + 'px',
+            left: (Math.random() * window.innerWidth) * 0.75 + 'px',
+          };
+          const newSize = Math.random() * 100 + 'px';
+
+          setPosition(newPosition);
+          setSize(newSize);
+        }, 6000); // Adjust the rotation delay as needed
+      }, 4000); // Adjust the initial delay as needed
+    }, 8000); // Adjust the total interval time as needed
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div
-      className={`space-robot ${direction}`}
-      style={{ top: position.top, left: position.left, width: size, height: size }}
+      className={`space-robot ${linearTransition ? 'linearTransition' : ''}`}
+      style={{
+        top: position.top,
+        left: position.left,
+        width: size,
+        height: size,
+        transform: `rotate(${rotation}deg)`,
+      }}
     >
       <img src={FloatingRobotImage} alt="Floating Robot" />
     </div>
